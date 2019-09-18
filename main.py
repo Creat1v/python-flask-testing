e358135ed6e4bc772b849fd833f9bae7f98cc041
Creat1v <mmorpgz1337@gmail.com>
2019-09-18 16:08:41 +0200

    Style adjustments according to pylint

--- main.py
+++ main.py
@@ -2,33 +2,36 @@
 # Based on https://pythonspot.com/login-authentication-with-flask/ and adjusted
 """Webserver via Flask for testing purpose"""
 
-from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+from flask import Flask, flash, render_template, request, session
 
 APP = Flask(__name__)
 
 
 @APP.route('/')
 def home():
+    """Root page"""
     if not session.get('logged_in'):
         return render_template('login_test.html')
-    else:
-        return "Hello Boss! <a href='/logout'>Logout</a>"
+
+    return "Hello Boss! <a href='/logout'>Logout</a>"
 
 
 @APP.route('/login', methods=['POST'])
 def do_admin_login():
+    """Login page/function"""
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
         session['logged_in'] = True
         return home()
         # return render_template('login_test.html')
-    else:
-        flash('wrong password!')
-        return home()
+
+    flash('wrong password!')
+    return home()
 
 
 @APP.route('/logout')
 def logout():
+    """Logout page/function"""
     session['logged_in'] = False
     return home()
 
